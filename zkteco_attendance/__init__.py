7889fe8e3241412c93e4f843f5d2e1920259b5bd
Eslamzeedan <eslamzeeddaan@gmail.com>
2026-07-14 13:54:55 +0300

    Update __init__.py

---
+++ zkteco_attendance/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2024 Your Organization
+# MIT License - See LICENSE file for details
+
+"""
+ZKTeco Attendance Integration for ERPNext v15
+
+Enterprise-grade biometric attendance synchronization solution.
+"""
+
+__version__ = "1.0.0"
+__app_name__ = "zkteco_attendance"
